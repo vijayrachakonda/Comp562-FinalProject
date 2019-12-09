2b6cfe54ef03ceca248c7e064ae3b62f2fd98103
njcolvin <njcolvin@live.unc.edu>
2019-12-09 18:54:02 -0500

    added print confusion matrix, weighted f1 scores

--- nbmodel.py
+++ nbmodel.py
@@ -9,10 +9,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import *
 
-df = pd.read_csv('./winemag-data-130k-v2.csv')
+df = pd.read_csv('../../Downloads/wine-reviews/winemag-data-130k-v2.csv')
 
 # Selecting only top ten win varieties to be classified
 top_ten = df['variety'].value_counts()[:10].index.tolist()
@@ -36,7 +37,13 @@ tokenizer = RegexpTokenizer(r'[a-zA-Z\']+')
 def tokenize(text):
     return [stemmer.stem(word) for word in tokenizer.tokenize(text.lower())]
 
-punc = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',"%", "flavor", "wine"]
+punc = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', "%", "flavor", "wine", 'abov', 'afterward', 'alon',
+'alreadi', 'alway', 'ani', 'anoth', 'anyon', 'anyth', 'anywher', 'becam', 'becaus', 'becom', 'befor', 'besid', 'cri', 'describ',
+'dure', 'els', 'elsewher', 'empti', 'everi', 'everyon', 'everyth', 'everywher', 'fifti', 'forti', 'henc', 'hereaft', 'herebi',
+'howev', 'hundr', 'inde', 'mani', 'meanwhil', 'moreov', 'nobodi', 'noon', 'noth', 'nowher', 'onc', 'onli', 'otherwis', 'ourselv',
+'perhap', 'pleas', 'sever', 'sinc', 'sincer', 'sixti', 'someon', 'someth', 'sometim', 'somewher', 'themselv', 'thenc', 'thereaft',
+'therebi', 'therefor', 'togeth', 'twelv', 'twenti', 'veri', 'whatev', 'whenc', 'whenev', 'wherea', 'whereaft', 'wherebi', 'wherev',
+'whi', 'yourselv', 'anywh', 'el', 'elsewh', 'everywh', 'ind', 'otherwi', 'plea', 'somewh']
 stops = text.ENGLISH_STOP_WORDS.union(punc)
 count_vect = CountVectorizer(stop_words=stops, tokenizer=tokenize)
 x_train_counts = count_vect.fit_transform(x_train)
@@ -58,14 +65,26 @@ x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 logistRegr = LogisticRegression()
 model_NB = MultinomialNB().fit(x_train_tfidf, y_train)
 model_LR = logistRegr.fit(x_train_tfidf, y_train)
+#clf = LinearDiscriminantAnalysis()
+#dense_x_train = x_train_tfidf.toarray()
+#model_GDA = clf.fit(dense_x_train, y_train)
+
+
 
 x_test_counts = count_vect.transform(x_test)
 x_test_tfidf = tfidf_transformer.transform(x_test_counts)
 
 predicted_varieties_NB = model_NB.predict(x_test_tfidf)
 predicted_varieties_LR = model_LR.predict(x_test_tfidf)
+#predicted_varieties_GDA = model_GDA.predict(x_test_tfidf)
+
+
 
 print("Accuracy of Multinomial Naive Bayes: ", accuracy_score(y_test, predicted_varieties_NB))
 print("Accuracy of Logistic Regression: ", accuracy_score(y_test, predicted_varieties_LR))
 print(classification_report(y_test, predicted_varieties_NB))
 print(classification_report(y_test, predicted_varieties_LR))
+#print('Weighted F1 Score of Multinomial Naive Bayes: ', f1_score(y_test, predicted_varieties_NB, average='weighted'))
+#print('Weighted F1 Score of Logistic Regression: ', f1_score(y_test, predicted_varieties_LR, average='weighted'))
+print('Confusion Matrix of Multinomial Naive Bayes: ', confusion_matrix(y_test, predicted_varieties_NB))
+print('Confusion Matrix of Logistic Regression: ', confusion_matrix(y_test, predicted_varieties_LR))
